398860b9e0f6f47037c1580c5e407f28cb707fba
RileyBarrett1 <rileybarrett7@gmail.com>
2024-09-18 09:33:14 -0230

    .

---
+++ src/main/java/ca/nl/cna/riley/barrett/funwitharthimeticoperaters.java
@@ -0,0 +1,26 @@
+package ca.nl.cna.riley.barrett;
+
+import java.util.Scanner;
+
+public class funwitharthimeticoperaters {
+    public static void main(String[] args) {
+        System.out.println("Fun with arthimetic operaters");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements you wish to enter: ");
+
+        int y = input.nextInt();
+        System.out.println("Enter the number of elements you wish to enter: ");
+        int x = input.nextInt();
+
+        System.out.printf("\n %d + %d = %d ",x , y, x+y);
+
+        System.out.printf("\n %d * %d = %d ",x , y, x*y);
+
+        System.out.printf("\n %d / %d = %d ",x , y, x/y);
+
+        System.out.printf("\n %d - %d = %d ",x , y, x-y);
+
+    }
+}
