239369caee7b315090c22430deba6840dd3170a5
RileyBarrett1 <rileybarrett7@gmail.com>
2024-09-18 09:31:24 -0230

    .

---
+++ src/main/java/ca/nl/cna/riley/barrett/FunWithIfs.java
@@ -0,0 +1,31 @@
+package ca.nl.cna.riley.barrett;
+
+import java.util.Scanner;
+
+public class FunWithIfs {
+    public static void main(String[] args) {
+        System.out.println("Fun with arthimetic operaters");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements you wish to enter: ");
+
+        int y = input.nextInt();
+        System.out.println("Enter the number of elements you wish to enter: ");
+        int x = input.nextInt();
+
+        if(x==y){
+            System.out.printf("%d ==%d",x,y);
+    }
+        if(x!=y){
+            System.out.printf("%d !=%d",x,y);
+        }
+        if(x<y){
+            System.out.printf("%d < %d",x,y);
+        }
+        if(x>y){
+            System.out.printf("%d > %d",x,y);
+        }
+}
+
+}
